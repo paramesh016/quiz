10616d2d462d986c37040fe81c27681932b6af04
Paramesh V <paramesh.v@amadeus.com>
2020-04-04 16:50:46 +0530

    models db

--- quiz/quizapp/models.py
+++ quiz/quizapp/models.py
@@ -1,8 +1,30 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
-
-class Questions(models.Model):
-    Questions = models.CharField()
+class QuizUser(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    experience = models.IntegerField(max_length=2)
     
+
+class Quiz(models.Model):
+    quiz_id = models.CharField(max_length=32)
+    quiz_title = models.CharField(max_length=32)
+
+
+class Question(models.Model):
+    quiz = models.ForeignKey(Quiz)
+    questions = models.CharField(blank=False)
+    score = models.FloatField(default=1)
+
+
+class Option(models.Model):
+    questions = models.ForeignKey(Question)
+    option = models.ForeignKey(quiz)
+    is_correct = models.BooleanField(default=False)
+
+
+class Answers(models.Model):
+    user = models.ForeignKey(QuizUser)
+    answers = models.ForeignKey(Option)
